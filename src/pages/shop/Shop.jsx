e5283d33d03c7b6c99e53a2aa31f2c3beb32f947
Dayer <dayerxr.cher@gmail.com>
2025-11-06 13:31:02 +0800

    Added background styles for cart and shop pages

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -3,7 +3,7 @@ import Navbar from "/src/components/navbar/Navbar.jsx"
 export default function Shop() {
 
     return(
-        <div>
+        <div className="bg-linear-to-br from-pink-500 to-sky-600 h-screen">
             <Navbar></Navbar>
         </div>
     )
