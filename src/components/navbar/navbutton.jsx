275f418545a7a86707794d0f624423d9c26f9cab
Dayer <dayerxr.cher@gmail.com>
2025-11-06 14:29:44 +0800

    Added styling for navButton

--- src/components/navbar/navbutton.jsx
+++ src/components/navbar/navbutton.jsx
@@ -6,7 +6,7 @@ export default function NavButton({name}) {
         <Link className="" to={name}>
             <button className="font-MomoSignature rounded-3xl p-4 text-2xl
             mx-15 bg-neutral-400/60 hover:bg-slate-900 hover:text-white hover:scale-115
-            transition">{name}</button>
+            transition px-7">{name}</button>
         </Link>
     )
 }
