638a34e6ac1d46c8ced6015ddabe404b30bb5b73
Dayer <dayerxr.cher@gmail.com>
2025-11-06 13:31:42 +0800

    Added styling for navbar and navbutton components

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -3,7 +3,7 @@ import NavButton from './NavButton.jsx'
 export default function Navbar(){
     
     return(
-        <div className="">
+        <div className="mt-5">
             <NavButton name="Home"></NavButton>
             <NavButton name="Shop"></NavButton>
             <NavButton name="Cart"></NavButton>
--- src/components/navbar/navbutton.jsx
+++ src/components/navbar/navbutton.jsx
@@ -4,7 +4,9 @@ export default function NavButton({name}) {
 
     return(
         <Link className="" to={name}>
-            <button className="rounded-xxl p-4 text-2xl mx-15 hover:bg-">{name}</button>
+            <button className="font-MomoSignature rounded-3xl p-4 text-2xl
+            mx-15 bg-neutral-400/60 hover:bg-slate-900 hover:text-white hover:scale-115
+            transition">{name}</button>
         </Link>
     )
 }
