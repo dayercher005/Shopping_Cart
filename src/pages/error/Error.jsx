24e305e020db715526cf9f8de3523de83e276b5d
Dayer <93074807+dayercher005@users.noreply.github.com>
2025-11-06 22:53:35 +0800

    Styling Error Page

--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -3,10 +3,11 @@ import { Link } from "react-router"
 export default function Error() {
 
     return(
-        <div>
+        <div className="h-screen bg-linear-to-br from-pink-500 to-sky-600">
             <h1>Oh no, this page doesn't exist!</h1>
             <Link to = "/">
-                <button className="">Back to Home</button>
+                <button className="rounded-2xl bg-neutral-400/60 hover:bg-slate-900 hover:text-white
+                p-2">Back to Home</button>
             </Link>
         </div>
     )
