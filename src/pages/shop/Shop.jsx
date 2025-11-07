9e496e351509ef1f49c5cb5f59bb1569f6b3ba0b
Dayer <93074807+dayercher005@users.noreply.github.com>
2025-11-07 22:01:18 +0800

    Retrieving data from FakeStoreAPI

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -2,6 +2,39 @@ import {useState, useEffect} from "react"
 
 export default function Shop() {
 
+    const [shop, setShop] = useState([])
+
+    useEffect(() => {
+
+        let ignore = false
+
+        async function APICaller() {
+            try{
+                for(let index = 0; index < 24; index ++){
+                    let itemScript = await fetch(`https://fakestoreapi.com/products/${index}`)
+                    let itemData = await itemScript.json()
+                    let itemName = itemData.title
+                    let itemPrice = itemData.price
+                    let itemDescription = itemData.description
+                    let itemImage = itemData.image
+
+                    if (!ignore){
+                        setShop([...shop, {name: itemName, price: itemPrice, description: itemDescription, image: itemImage}])
+                    }
+                }
+            }
+            catch(error){
+                return error
+            }
+        }
+
+        APICaller()
+
+        return(
+            ignore = true
+        )
+    }, [])
+
     return(
         <div className="bg-linear-to-br from-pink-500 to-sky-600 h-screen">
 
