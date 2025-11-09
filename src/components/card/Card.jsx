5092b47287d2562285c380a9db3c719b99706cb9
Dayer <93074807+dayercher005@users.noreply.github.com>
2025-11-09 11:36:38 +0800

    Adding components to Card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -4,11 +4,28 @@ export default function Card({name, price, image}){
 
     const [quantity, setQuantity] = useState(0)
 
+    const addQuantity = () => {
+        setQuantity(quantity => quantity += 1)
+    }
+
+    const reduceQuantity = () => {
+        if (quantity <= 0){
+            return
+        }
+
+        setQuantity(quantity => quantity -= 1)
+    }
+
     return(
         <div className="bg-white rounded-2xl hover:scale-105 w-100 transition">
             <h1 className="text-4xl">{name}</h1>
             <img className="size-50" src={image}></img>
             <p className="text-3xl text-center">USD {price}</p>
+            <div className="flex justify-center">
+                <button className="" onClick={addQuantity}> + </button>
+                <p className="">{quantity}</p>
+                <button className="" onClick={reduceQuantity}> - </button>
+            </div>
         </div>
     )
 }
