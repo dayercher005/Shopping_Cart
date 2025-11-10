4eca6615989262cb06860f7b35aa9953caed17ad
Dayer <dayerxr.cher@gmail.com>
2025-11-10 14:55:42 +0800

    Using outlet context from react routing to render components

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -22,12 +22,13 @@ export default function Card({name, price, image}){
             <h1 className="text-3xl basis-1/3 font-Caveat text-center px-2">{name}</h1>
             <img className="size-35 basis-1/3 mx-auto" src={image}></img>
             <p className="text-3xl basis-1/6 text-center font-Caveat">USD {price}</p>
-            <div className="flex basis-1/6 justify-center gap-10 py-5">
-                <button className="rounded-full bg-green-400 size-10 text-2xl text-center hover:bg-green-500 hover:scale-110 transition hover:shadow-xl" onClick={addQuantity}> + </button>
-                <p className="text-center font-Caveat text-4xl">Qty: {quantity}</p>
-                <button className="rounded-full bg-green-400 size-10 text-2xl text-center hover:bg-green-500 hover:scale-110 transition hover:shadow-xl" onClick={reduceQuantity}> - </button>
+            <div className="flex basis-1/6 justify-center items-center">
+                <button className="rounded-full bg-green-400 size-10 text-2xl text-center mx-2.5 hover:bg-green-500 hover:scale-110 transition hover:shadow-xl" onClick={addQuantity}> + </button>
+                <label className="font-Caveat text-4xl">Qty: </label>
+                <input type="number" value={quantity} onChange={(event) => setQuantity(event.target.value)} className="h-8 w-15 font-Caveat text-2xl px-2 mx-3"/>
+                <button className="rounded-full bg-green-400 size-10 text-2xl text-center mx-2.5 hover:bg-green-500 hover:scale-110 transition hover:shadow-xl" onClick={reduceQuantity}> - </button>
             </div>
-            <CartButton></CartButton>
+            <CartButton name={name} price={price} quantity={quantity}></CartButton>
         </div>
     )
 }
