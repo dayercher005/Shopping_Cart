d5892d93ab18c34c1e483ba8bac67c340947879c
Dayer <93074807+dayercher005@users.noreply.github.com>
2025-11-13 23:56:11 +0800

    Added feature of removing items from Cart

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,26 +1,18 @@
 import { useOutletContext } from 'react-router-dom'
 import Receipt from '/src/components/receipt/Receipt.jsx'
-import binIcon from '/src/assets/bin.svg'
+import Item from '/src/components/receipt/Item.jsx'
 
 export default function Cart() {
 
     const [cartItems, setCartItems] = useOutletContext();
 
-    const CartItemsRemover = () => {
-        setCartItems(cartItems => cartItems.filter(item => item.name !== item.name))
+    const CartItemsRemover = (name) => {
+        setCartItems(cartItems => cartItems.filter(item => item.name !== name))
     }
 
     const cartList = cartItems.map(item => 
-        <div className="flex justify-center">
-            <h1 className="flex-4/10 font-Caveat text-3xl p-2.5">{item.name}</h1>
-            <p className="flex-3/10 font-Caveat text-3xl p-2.5">{item.quantity}</p>
-            <p className="flex-2/10 font-Caveat text-3xl p-2.5">USD {item.quantity * item.price}</p>
-            <div className="flex-2/10 text-center">
-                <button onClick={CartItemsRemover} className=" rounded-xl border-2 p-1.5 mt-1.5 hover:scale-110 hover:bg-slate-300 transition">
-                    <img className=" size-7" src={binIcon} />
-                </button>
-            </div>
-        </div>
+        <Item name={item.name} quantity={item.quantity} price={item.price} CartItemsHandler={CartItemsRemover}>
+        </Item>
     )
 
 
